0f3615f48e52bf32738092d198a5191335927492
intelia <Intelia@163.com>
2020-08-04 12:06:03 +0800

    Flink批处理

---
+++ FlinkTest/src/main/scala/com/intelia/wc/WordCount.scala
@@ -0,0 +1,13 @@
+package com.intelia.wc
+
+/**
+ * @description TODO
+ * @auther Intelia
+ * @date 2020.8.4 12:03
+ * @mogified By:
+ */
+object WordCount{
+    def main(args: Array[String]): Unit = {
+
+    }
+}
